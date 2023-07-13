d13675c56999d4812689c4c258ddafd9acfdbf8e
Rahul9099 <rahuldange208@gmail.com>
2023-07-13 19:21:39 +0530

    working on showing the map next work on showing the text input field and user input

--- script.js
+++ script.js
@@ -10,3 +10,42 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+navigator.geolocation.getCurrentPosition(
+  position => {
+    const { latitude, longitude } = position.coords;
+    
+    console.log(
+      `https://www.google.pt/maps/@${latitude},${longitude}`
+    );
+    const map = L.map('map').setView([latitude, longitude], 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    L.marker([latitude, longitude])
+      .addTo(map)
+      .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+      .openPopup();
+    map.on('click',(event)=>{
+      const {lat,lng} = event.latlng
+      console.log(lat,lng);
+      L.marker([lat, lng])
+      .addTo(map)
+      .bindPopup(L.popup({
+        maxWidth:250,
+        minWidth:100,
+        autoClose:false,
+        closeOnClick:false,
+        className:'running-popup'
+      }))
+      .setPopupContent('rahul')
+      .openPopup();
+    })
+  },
+  err => {
+    console.log(`error occur ${err}`);
+  }
+);

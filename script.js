5145d0c2354d3c6430e9e9dc3f9e886c384cc5aa
Rahul9099 <rahuldange208@gmail.com>
2023-07-14 10:44:40 +0530

    class app is now refactor learn about the binding of this with eventlistner

--- script.js
+++ script.js
@@ -11,41 +11,70 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-navigator.geolocation.getCurrentPosition(
-  position => {
+let map , mapEvent;
+
+class App{
+  #map;
+  #mapEvent;
+  constructor(){
+  this._getPostion();
+  form.addEventListener('submit',this._newWorkout.bind(this));
+  inputType.addEventListener('change',this._toogleElevationField)
+ }
+
+ _getPostion(){
+  navigator.geolocation.getCurrentPosition(
+    this._loadMap.bind(this),
+    err => {
+      console.log(`error occur ${err}`);
+    }
+  );
+  
+ }
+ _loadMap(position){
     const { latitude, longitude } = position.coords;
-    
     console.log(
       `https://www.google.pt/maps/@${latitude},${longitude}`
     );
-    const map = L.map('map').setView([latitude, longitude], 13);
+     this.#map = L.map('map').setView([latitude, longitude], 13);
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    }).addTo(map);
+    }).addTo(this.#map);
 
-    L.marker([latitude, longitude])
-      .addTo(map)
-      .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-      .openPopup();
-    map.on('click',(event)=>{
-      const {lat,lng} = event.latlng
-      console.log(lat,lng);
-      L.marker([lat, lng])
-      .addTo(map)
-      .bindPopup(L.popup({
-        maxWidth:250,
-        minWidth:100,
-        autoClose:false,
-        closeOnClick:false,
-        className:'running-popup'
-      }))
-      .setPopupContent('rahul')
-      .openPopup();
-    })
-  },
-  err => {
-    console.log(`error occur ${err}`);
-  }
-);
+    this.#map.on('click',this._showForm.bind(this))
+ }
+ _showForm(event){
+    this.#mapEvent=event;
+    form.classList.remove('hidden');
+    inputDistance.focus();
+ }
+
+ _toogleElevationField(){
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden')
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden')
+ }
+
+ _newWorkout(e){
+  e.preventDefault();
+  const {lat,lng} = this.#mapEvent.latlng
+  console.log(lat,lng);
+  L.marker([lat, lng])
+  .addTo(this.#map)
+  .bindPopup(L.popup({
+    maxWidth:250,
+    minWidth:100,
+    autoClose:false,
+    closeOnClick:false,
+    className:'running-popup'
+  }))
+  .setPopupContent('rahul')
+  .openPopup();
+ }
+}
+
+const app =new App();
+
+
+
